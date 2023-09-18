76f9857529e457156fcd928f7c5b052b55cb2e58
Andy Patari <andy.patari@gmail.com>
2023-09-18 09:15:27 -0400

    passing tests

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
 def write_file(file_name, file_content):
-    pass
+    formatted_file_name = file_name.with_suffix(".txt")
+    with open(formatted_file_name, mode = 'w', encoding="utf-8") as text_file:
+        text_file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    formatted_file_name = file_name.with_suffix(".txt")
+    with open(formatted_file_name, mode = 'a', encoding="utf-8") as text_file:
+        text_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    formatted_file_name = file_name.with_suffix(".txt")
+    with open(formatted_file_name, mode = 'r', encoding="utf-8") as text_file:
+        return text_file.read()
+
